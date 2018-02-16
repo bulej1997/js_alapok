9cf591c44259aeed91a23e58e8bb9df368268551
Unknown <bulej1997@gmail.com>
2018-02-16 09:31:07 +0100

    jsalapokgyak

--- app.js
+++ app.js
@@ -3,8 +3,19 @@
 /*jslint indent: 2 */
 
 console.log("Hello world");
-
 $('.image-popup').magnificPopup({
-    type: 'image'
-gallery:  {enabled: true}
+    type: 'image',
+    gallery: {enabled: true}
 });
+
+//minden image-popup class-al rendelkező elemet elrejtünk
+//sima javascriptet használunk ehhez:
+[].forEach.call(document.querySelectorAll('.image-popup'),
+    function (el) {
+    el.style.display = 'none';   
+});
+
+//ez ugyanazt csinálja mint a fentebbi kód, csak ez
+//a jquerry library segítségével:
+
+//$('.image-popup').css('display', 'none');
